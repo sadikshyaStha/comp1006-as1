dfe62666e2b29f2597b9c5cf45e8170860115389
sadikshyaStha <200563573@student.georgianc.on.ca>
2024-04-02 18:59:19 -0400

    index page

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
     include('shared/header.php'); 
     ?> <!--Home page title-->
     <h2>Welcome to Tracking of Household Daily Chores</h2>
+    <h3>this is the homepage of my webste. The purpose of my website is so that
+        every member of house can easily view all th chores that are there and that there 
+        there to be done. it willgit also help them to keep track of their time.
+         this is the extensiion of assignment 1. In this website, i will add header
+          that will have a navigation bar so that user can go to evey page easily </h3>
 </main>
 </body>
 </html>
